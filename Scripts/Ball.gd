7cee89f249952cc885052209e09f32990f4ed1c8
Ethan Karp <ethankarp5@gmail.com>
2020-11-29 10:20:10 -0800

    part 3

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,11 +1,24 @@
 extends RigidBody2D
 
+const SPEEDUP = 400
+const MAXSPEED = 30000
+
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
+	delta
 	var bodies = get_colliding_bodies()
 	
 	for body in bodies:
 		if body.is_in_group("bricks"):
 			body.queue_free()
+		
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = position - body.get_node("Anchor").get_global_position()
+			var velocity = direction.normalized()*min(speed+SPEEDUP*delta, MAXSPEED*delta)
+			set_linear_velocity(velocity)
+	
+	if position.y > get_viewport_rect().end.y:
+		queue_free()
